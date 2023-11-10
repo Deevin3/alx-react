f6d2b2d6483c7a0dfed546deb0fd2d42d45997da
unknown <oviedodivine@gmail.com>
2023-11-10 07:16:42 -0800

    first commit

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+import HolbertonLogo from "./Holberton.jpg";
 import './App.css';
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <>
       <div className="App-header">
-        <img src={holbertonLogo} alt="Holberton logo" />
+        <img src={HolbertonLogo} alt="Holberton logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </div>
     </>
   );
---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Notification.css';
+
+const Notifications = () => {
+        return (
+            <div className='Notifications'> <p>Here is the list of notifications</p></div>
+        )
+}
+
+export default Notifications;
