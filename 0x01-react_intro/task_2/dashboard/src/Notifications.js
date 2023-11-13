00891ec40c04bdfe05c4c79b04bd280899edc31b
unknown <oviedodivine@gmail.com>
2023-11-13 04:59:15 -0800

    first commit

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './Notification.css';
+import {getLatestNotification} from "./utils"
 
 const Notifications = () => {
         return (
-            <div className='Notifications'> <p>Here is the list of notifications</p></div>
+            <div className='Notifications'> 
+            <button type="button" style="float: right;" aria-label="Close" onclick="console.log('Close button has been clicked')" img src="close-icon.png"></button>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data="default">New course available</li>
+                <li data="urgent">New resume available</li>
+                <li
+                  data="urgent"
+                  dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+                ></li>
+            </ul>
+            </div>
         )
 }
 
